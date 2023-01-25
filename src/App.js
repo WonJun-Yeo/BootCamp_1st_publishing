3479aaae92c583565f9911a181e7a40efc3d4ea0
WonJun Yeo <rbgh004@gmail.com>
2023-01-25 10:27:05 +0900

    Revert "<feat> add login and signup layout"
    This reverts commit 46bb04d7d5599cabb84f039c959439e2a9cb8549.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import './style/common/header.css';
 import './style/common/banner.css';
-import './style/common/login.css';
 import './style/reset.css';
 import './style/freeBoard.css';
 import './style/notice.css';
-import './style/qAndA.css';
-import './style/signup.css';
 
 import Header from './common/Header';
 import NoticeContainer from './notice/NoticeContainer';
 import Footer from './common/Footer';
 import Banner from './common/Banner';
-import QAndAView from './qAndA/QAndAView';
-import Signup from './signup/Signup';
-import Login from './common/Login';
 
 function App() {
   return (
@@ -22,10 +16,7 @@ function App() {
       <Header />
       <Banner />
       {/* <FreeboardContainer /> */}
-      {/* <NoticeContainer /> */}
-      {/* <QAndAView /> */}
-      {/* <Signup /> */}
-      <Login />
+      <NoticeContainer />
       <Footer />
     </div>
   );
